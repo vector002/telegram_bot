c8fd3ed600ddbafc5ccfc40e24020b6e4653df4c
ANVARJONOV NURMUHAMMAD <shahlohonteshaboyov@gmail.com>
2025-04-16 19:51:59 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,9 @@ from tarjimon import lotin_to_kirill, kirill_to_lotin
 
 TOKEN = '7854063099:AAHJ49P5QICWdpsbSxA_v9HBiKfHNJZt0Qo'  # o'z tokeningizni yozing
 bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Assalomu alaykum! Matn kiriting — bot uni avtomatik tarjima qiladi (lotin ↔ kiril).")
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
