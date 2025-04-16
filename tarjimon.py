fbe4e6d148dcf100a8242cae2a0cbcb8be31ad4c
ANVARJONOV NURMUHAMMAD <shahlohonteshaboyov@gmail.com>
2025-04-16 20:02:05 +0500

    Update tarjimon.py

--- tarjimon.py
+++ tarjimon.py
@@ -2,30 +2,36 @@
 
 # Lotin yozuvini Kirilga o'zgartirish
 def lotin_to_kirill(text):
-    text = text.lower()
-    sozlash = {
-        'yo': 'ё', 'ya': 'я', 'yu': 'ю', 'sh': 'ш', 'ch': 'ч', 'o‘': 'ў', 'g‘': 'ғ',
-        'a': 'а', 'b': 'б', 'd': 'д', 'e': 'е', 'f': 'ф', 'g': 'г', 'h': 'ҳ', 'i': 'и',
-        'j': 'ж', 'k': 'к', 'l': 'л', 'm': 'м', 'n': 'н', 'o': 'о', 'p': 'п', 'q': 'қ',
-        'r': 'р', 's': 'с', 't': 'т', 'u': 'у', 'v': 'в', 'x': 'х', 'y': 'й', 'z': 'з',
-        "'": 'ъ'
+    # Oddiy misol, siz to'liq o'zgartirish jadvalini ishlatishingiz mumkin
+    almashtirishlar = {
+        'a': 'а', 'b': 'б', 'd': 'д', 'e': 'е', 'f': 'ф', 'g': 'г',
+        'h': 'ҳ', 'i': 'и', 'j': 'ж', 'k': 'к', 'l': 'л', 'm': 'м',
+        'n': 'н', 'o': 'о', 'p': 'п', 'q': 'қ', 'r': 'р', 's': 'с',
+        't': 'т', 'u': 'у', 'v': 'в', 'x': 'х', 'y': 'й', 'z': 'з',
+        'ʼ': 'ъ', "'": 'ъ', 'ʺ': 'ъ', 'g‘': 'ғ', 'o‘': 'ў', 'sh': 'ш', 'ch': 'ч'
     }
-    for lot, kir in sorted(sozlash.items(), key=lambda x: -len(x[0])):
-        text = text.replace(lot, kir)
+
+    for lotin, kiril in sorted(almashtirishlar.items(), key=lambda x: -len(x[0])):
+        text = text.replace(lotin, kiril)
+        text = text.replace(lotin.capitalize(), kiril.capitalize())
+        text = text.replace(lotin.upper(), kiril.upper())
+
     return text
 
-# Kiril yozuvini Lotinga o'zgartirish
 def kirill_to_lotin(text):
-    text = text.lower()
-    sozlash = {
-        'ё': 'yo', 'я': 'ya', 'ю': 'yu', 'ш': 'sh', 'ч': 'ch', 'ў': "o‘", 'ғ': "g‘",
-        'а': 'a', 'б': 'b', 'д': 'd', 'е': 'e', 'ф': 'f', 'г': 'g', 'ҳ': 'h', 'и': 'i',
-        'ж': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'қ': 'q',
-        'р': 'r', 'с': 's', 'т': 'т', 'у': 'u', 'в': 'v', 'х': 'x', 'й': 'y', 'з': 'z',
-        'ъ': "'"
+    almashtirishlar = {
+        'а': 'a', 'б': 'b', 'д': 'd', 'е': 'e', 'ф': 'f', 'г': 'g',
+        'ҳ': 'h', 'и': 'i', 'ж': 'j', 'к': 'k', 'л': 'l', 'м': 'm',
+        'н': 'n', 'о': 'o', 'п': 'p', 'қ': 'q', 'р': 'r', 'с': 's',
+        'т': 't', 'у': 'u', 'в': 'v', 'х': 'x', 'й': 'y', 'з': 'z',
+        'ъ': "'", 'ғ': 'g‘', 'ў': 'o‘', 'ш': 'sh', 'ч': 'ch'
     }
-    for kir, lot in sorted(sozlash.items(), key=lambda x: -len(x[0])):
-        text = text.replace(kir, lot)
+
+    for kiril, lotin in sorted(almashtirishlar.items(), key=lambda x: -len(x[0])):
+        text = text.replace(kiril, lotin)
+        text = text.replace(kiril.capitalize(), lotin.capitalize())
+        text = text.replace(kiril.upper(), lotin.upper())
+
     return text
 
 # Katta matnni bo'lib tarjima qilish
